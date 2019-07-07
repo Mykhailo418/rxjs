e88cc7122133b60959de1f8c4a50f62930bf8e0c
Mykhailo418 <smmichael418@gmail.com>
2019-07-07 23:23:01 +0300

    interval, timer, fromEvent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { timer, interval, fromEvent } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+	count: number;
+	mouseX: number = 0;
+	mouseY: number = 0;
 
+	ngOnInit(){
+		const interval$ = interval(1000); // like a setInterval()
+		const timer$ = timer(3000, 1000);
+		const clickEvent$ = fromEvent(document, 'click');
+
+
+		timer$.subscribe( (val: number) => this.count = val);
+		clickEvent$.subscribe( (event: MouseEvent) => {
+			this.mouseX = event.clientX;
+			this.mouseY = event.clientY;
+		});
+	}
 }
