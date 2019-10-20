bff5462f46bdff798cb87a3ac3cb8ac77fca853b
Mykhailo418 <smmichael418@gmail.com>
2019-10-20 13:53:06 +0300

    shareReplay operator

--- src/app/components/repos/repos.component.ts
+++ src/app/components/repos/repos.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {CustomObservableService} from '../../services/custom-observable.service';
 import {Repo} from '../../models/repo';
 import {Observable} from 'rxjs';
-import {map} from 'rxjs/operators';
+import {map, tap, shareReplay} from 'rxjs/operators';
 
 @Component({
   selector: 'app-repos',
@@ -16,7 +16,10 @@ export class ReposComponent implements OnInit {
   constructor(private reposService: CustomObservableService) { }
 
   loadRepos(){
-    const repos$ = this.reposService.getRepos();
+    const repos$ = this.reposService.getRepos().pipe(
+      tap(() => console.log('Repos was loaded!')),
+      shareReplay() // in order prevent multiple request to server, make only one request and share copy of data to other subscribers
+    );
     this.privateRepos$ = repos$.pipe(
       map((repos: Repo[]) => repos.filter(repos => repos.private))
     );
