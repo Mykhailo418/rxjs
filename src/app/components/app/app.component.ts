69d1f48c223505ebd526987f485900e9b956de3d
Mykhailo418 <smmichael418@gmail.com>
2019-10-20 20:11:57 +0300

    merge

--- src/app/components/app/app.component.ts
+++ src/app/components/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { timer, interval, fromEvent, of } from 'rxjs';
-import { concat } from 'rxjs/operators';
+import { concat, map, merge } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -38,14 +38,27 @@ export class AppComponent implements OnInit {
     setTimeout(() => timerSubscription.unsubscribe(), 9000);
 
     this.concatOperator();
+    this.mergeOperator();
 	}
 
   concatOperator(){
     const souce1$ = of(1, 2);
     const souce2$ = of(3, 4);
     const souce3$ = of(5, 6);
-
+    // concat - combine onservables in one run them one by one
+    // after source1 complete -> start source2, after source2 complete -> start source3
     souce1$.pipe(concat(souce2$, souce3$))
       .subscribe((val: number) => console.log('concatOperator: ', val));
+  }
+
+  mergeOperator(){
+    const interval1$ = interval(1000);
+    const interval2$ = interval1$.pipe(map((val: number) => val*10));
+    // merge - combine observables in one that runs at the same time
+    const mergeSubscribtion = interval1$.pipe(
+      merge(interval2$)
+    )
+    .subscribe((val: number) => console.log('mergeOperator: ', val));
+    setTimeout(() => mergeSubscribtion.unsubscribe(), 5000);
   }
 }
